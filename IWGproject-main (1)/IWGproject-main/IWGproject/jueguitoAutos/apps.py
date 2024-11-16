23b357c3f8bcb5ec23c6bea4a8486694bf698853
pellitoh <133620821+pellitoh@users.noreply.github.com>
2024-11-15 22:28:44 -0300

    Delete IWGproject-main (1)/IWGproject-main directory

--- IWGproject-main (1)/IWGproject-main/IWGproject/jueguitoAutos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JueguitoautosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jueguitoAutos'
--- IWGproject-main (1)/IWGproject-main/README.md
+++
@@ -1,4 +0,0 @@
-# IWG1
-ingenieria
-intentando no echarnos el ramo xdxd
-el primero en leer esto se cmabia a comercial
