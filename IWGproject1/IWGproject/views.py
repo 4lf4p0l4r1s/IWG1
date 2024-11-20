4a9356b6c4bec39ca6ae0889d4dee147e85107d5
pellitoh <133620821+pellitoh@users.noreply.github.com>
2024-11-19 23:38:35 -0300

    Delete IWGproject1 directory

--- IWGproject1/IWGproject/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    # lógica para la vista home
-    return render(request, 'home.html')
-
--- IWGproject1/usuarios/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('registro_usuario/', views.registro_usuario, name='registro_usuario'),
-    path('registrar_kilometraje/', views.registrar_kilometraje, name='registrar_kilometraje'),
-]
